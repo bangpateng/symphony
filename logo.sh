2d2837a00c2dce68d8bce9bbe5cef13b9ae8bd18
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2024-08-12 14:11:48 +0700

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define color codes
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+# Display the logo and text with the same color
+echo -e "${BLUE}"
+echo "░█▀▄░█▀█░█▀█░█▀▀        "
+echo "░█▀▄░█▀█░█░█░█░█        "
+echo "░▀▀░░▀░▀░▀░▀░▀▀▀        "
+echo "░█▀█░█▀█░▀█▀░█▀▀░█▀█░█▀▀"
+echo "░█▀▀░█▀█░░█░░█▀▀░█░█░█░█"
+echo "░▀░░░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀"
+echo ""
+echo "Name Testnet : Symphony"
+echo "Follow Telegram : @bangpateng_airdrop"
+echo "Website :https://bangpateng.xyz"
+echo -e "${NC}"
